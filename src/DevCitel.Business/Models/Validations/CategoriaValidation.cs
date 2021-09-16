5a904359805cf5dd1f6908d5e0a700e6eece8d84
Jefferson Luiz <stylle_musico@hotmail.com>
2021-09-16 20:14:24 -0300

    Implementando DTOs

---
+++ src/DevCitel.Business/Models/Validations/CategoriaValidation.cs
@@ -0,0 +1,15 @@
+﻿using FluentValidation;
+
+namespace DevCitel.Business.Models.Validations
+{
+    public class CategoriaValidation : AbstractValidator<Categoria>
+    {
+        public CategoriaValidation()
+        {
+
+            RuleFor(c => c.Descricao)
+                .NotEmpty().WithMessage("O campo {PropertyName} precisa ser fornecido")
+                .Length(2, 80).WithMessage("O campo {PropertyName} precisa ter entre {MinLength} e {MaxLength} caracteres");
+        }
+    }
+}
