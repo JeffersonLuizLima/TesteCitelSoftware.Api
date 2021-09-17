6fd995b8c41885787028178ff5eeadcdc6d492a6
Jefferson Luiz <stylle_musico@hotmail.com>
2021-09-17 18:37:00 -0300

    Validação nome  da categoria

--- src/DevCitel.Business/Services/CategoriaService.cs
+++ src/DevCitel.Business/Services/CategoriaService.cs
@@ -1,6 +1,7 @@
 ﻿using DevCitel.Business.Intefaces;
 using DevCitel.Business.Models;
 using DevCitel.Business.Models.Validations;
+using System.Linq;
 using System.Threading.Tasks;
 
 namespace DevCitel.Business.Services
@@ -19,12 +20,24 @@ namespace DevCitel.Business.Services
         {
             if (!ExecutarValidacao(new CategoriaValidation(), categoria)) return;
 
+            if (_categoriaRepository.Buscar(c => c.Descricao == categoria.Descricao).Result.Any())
+            {
+                Notificar("Já existe uma categoria com este nome informado.");
+                return;
+            }
+
             await _categoriaRepository.Adicionar(categoria);
         }
 
         public async Task Atualizar(Categoria categoria)
         {
             if (!ExecutarValidacao(new CategoriaValidation(), categoria)) return;
+
+            if (_categoriaRepository.Buscar(c => c.Descricao == categoria.Descricao && c.Id != categoria.Id).Result.Any())
+            {
+                Notificar("Já existe uma categoria com este nome informado.");
+                return;
+            }
 
             await _categoriaRepository.Atualizar(categoria);
         }
